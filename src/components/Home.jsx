b39800bdcb258eb7b408d15afa0b3e2fcc8c40aa
rajputanuj31 <115709571+rajputanuj31@users.noreply.github.com>
2023-03-02 23:29:50 +0530

    First commit

---
+++ src/components/Home.jsx
@@ -0,0 +1,56 @@
+import React, { useEffect, useState } from 'react'
+import Task from './Task'
+
+const Home = () => {
+    const intialArr = localStorage.getItem("tasks")?JSON.parse(localStorage.getItem("tasks")):[];
+    const [tasks, setTasks] = useState(intialArr);
+    const [title, setTitle] = useState("");
+    const [description, setDescription] = useState("");
+
+    const submitHandler = (e) => {
+        e.preventDefault();
+        if (title.length!==0 && description.length!==0) {
+            setTasks([...tasks, { title, description }])
+            setTitle("")
+            setDescription("")
+        }
+        else if(title.length===0 && description.length===0){
+            alert("Please Fill all Fields")
+        }
+        else if(description.length===0){
+            alert("Please Enter the some description")
+        }
+        else{
+            alert("Please Enter the Title")
+        }
+    };
+    const deleteTask = (index) => {
+        const filteredarr = tasks.filter((val, i) => {
+            return i !== index
+        })
+        setTasks(filteredarr);
+    };
+    useEffect(()=>{
+        localStorage.setItem("tasks",JSON.stringify(tasks));
+    },[tasks]);
+    return (
+        <div className='container'>
+            <h1>Daily Goals</h1>
+            <form onSubmit={submitHandler}>
+                <input type="text" placeholder='Title' value={title} onChange={(e) => setTitle(e.target.value)} />
+                <textarea placeholder='Description' value={description} onChange={(e) => setDescription(e.target.value)}></textarea>
+                <button type='submit'>ADD</button>
+            </form>
+            {tasks.map((item, index) => (
+                <Task key={index}
+                    title={item.title}
+                    description={item.description}
+                    deleteTask={deleteTask}
+                    index={index}
+                />
+            ))}
+        </div>
+    )
+}
+
+export default Home
