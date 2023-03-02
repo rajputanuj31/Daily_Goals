88f09f3f81443bc81d3d8c71546683a2ea6ca4a0
rajputanuj31 <115709571+rajputanuj31@users.noreply.github.com>
2023-03-02 19:16:50 +0530

    Updates

---
+++ precache-manifest.80550e0dfdefc1c40f6abec7eab378a9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "90ff93a806d8ace4cfb8",
+    "url": "/static/css/main.d323c837.chunk.css"
+  },
+  {
+    "revision": "90ff93a806d8ace4cfb8",
+    "url": "/static/js/main.90ff93a8.chunk.js"
+  },
+  {
+    "revision": "a99c263911670a2ba01f",
+    "url": "/static/js/1.a99c2639.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "38aa00f6629ef4f0443bd6952f5ff748",
+    "url": "/index.html"
+  }
+];
