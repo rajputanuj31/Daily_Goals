4a84d485a0cf849c2cd30fccfb24e3d50ba6bf27
rajputanuj31 <115709571+rajputanuj31@users.noreply.github.com>
2023-03-04 19:49:26 +0530

    Update Task.jsx

--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -1,12 +1,46 @@
-import React from 'react'
+import React,{useState} from 'react'
+import { BsFillCheckCircleFill,BsFillXCircleFill,BsFillDashCircleFill } from "react-icons/bs";
 const Task = ({title,description,deleteTask,index}) => {
+    const [mystyle,setMystyle] = useState({
+        opacity:1
+    })
+    const complete=()=>{
+        if (mystyle.opacity===1) {
+            setMystyle({
+                opacity:0.3,
+                backgroundColor:'green'
+            })
+        }
+        else{
+            setMystyle({
+                opacity:1,
+                backgroundColor:'white'
+            })
+        }
+    }
+    const Incomplete=()=>{
+        if (mystyle.opacity===1) {
+            setMystyle({
+                opacity:0.3,
+                backgroundColor:'red'
+            })
+        }
+        else{
+            setMystyle({
+                opacity:1,
+                backgroundColor:'white'
+            })
+        }
+    }
     return (
-        <div className='Task'>
+        <div className='Task' style={mystyle}>
             <div>
                 <p>{title}</p>
                 <span>{description}</span>
             </div>
-            <button onClick={()=>deleteTask(index)} >-</button>
+            <BsFillCheckCircleFill className='complete' onClick={complete}/>
+            <BsFillXCircleFill className='complete' onClick={Incomplete}/>
+            <BsFillDashCircleFill className='complete' onClick={()=>deleteTask(index)}/>
         </div>
     )
 }
